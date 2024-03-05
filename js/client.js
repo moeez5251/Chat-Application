d7f182c86ed33e94a403ac6cbabfe5344917a466
Moeez <159162874+moeez5251@users.noreply.github.com>
2024-03-05 18:34:03 +0500

    Update client.js

--- js/client.js
+++ js/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:8000/');
+
 const form = document.getElementById('send-container');
 const messageInp = document.getElementById('messageInp');
 const messageContainer = document.querySelector(".container")
